52cc421dbb661fbfccd64af6cde0c41f73f3ae76
Ethan <mimorozu@gmail.com>
2026-06-07 23:20:36 +0100

    fix prisma client import path and add postinstall

--- src/prisma.js
+++ src/prisma.js
@@ -1,8 +1,8 @@
+const { PrismaClient } = require('./generated/prisma')
 const { PrismaPg } = require('@prisma/adapter-pg')
-const { PrismaClient } = require('@prisma/client')
 
-const adapter = new PrismaPg({ 
-  connectionString: process.env.DATABASE_URL 
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL
 })
 
 const prisma = new PrismaClient({ adapter })
